9948cc058a21eaf6c7098d4e65e36d8bddce2262
Houria <126777373+Hou-ry@users.noreply.github.com>
2025-04-17 14:16:35 +0100

    Admin registration

--- travelApp/admin.py
+++ travelApp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Destination , booking , Subscribe
+from .models import Destination , booking , Subscribe , PromoCode
 
 # Register your models here.
 
 admin.site.register(Destination)
 admin.site.register(booking)
 admin.site.register(Subscribe)
+admin.site.register(PromoCode)
