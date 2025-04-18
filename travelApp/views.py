12eca49da57bb9e488f9e343e8f8120ce46c5ab9
Houria <126777373+Hou-ry@users.noreply.github.com>
2025-04-18 19:05:29 +0100

    Refactor booking summary template and view , to reflect model changes

--- travelApp/views.py
+++ travelApp/views.py
@@ -105,7 +105,7 @@ def bookingSummary(request , trip_id):
     if request.method =='POST':
         promoCodeInput = request.POST.get('promo', '').strip()
 
-    subtotal = float(trip.price )* numberOfTravelers
+    subtotal = float(trip.price_per_person )* numberOfTravelers
     discount = 0
     final_total = subtotal
     promo_error = None
