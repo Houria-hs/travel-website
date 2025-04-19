ff7417190bef9467846676b3543d50c1bcc23fa2
Houria <126777373+Hou-ry@users.noreply.github.com>
2025-04-19 20:37:05 +0100

    admin registration for PayementProof model

--- travelApp/admin.py
+++ travelApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Destination , booking , Subscribe , PromoCode
+from .models import Destination , booking , Subscribe , PromoCode , PayementProof
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(Destination)
 admin.site.register(booking)
 admin.site.register(Subscribe)
 admin.site.register(PromoCode)
+admin.site.register(PayementProof)
