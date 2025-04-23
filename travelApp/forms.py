741606cd04a6cc72a0b251b2c43a6f4e079d3242
Houria <126777373+Hou-ry@users.noreply.github.com>
2025-04-23 14:53:43 +0100

    Customize login form with French labels and improved messages

--- travelApp/forms.py
+++ travelApp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True, label="Adresse e-mail")
@@ -22,3 +22,11 @@ class RegisterForm(UserCreationForm):
         self.fields['password1'].label = "Mot de passe"
         self.fields['password2'].label = "Confirmation du mot de passe"
 
+
+class CustomLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = "Nom d’utilisateur"
+        self.fields['password'].label = "Mot de passe"
+
+    
