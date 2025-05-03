80ab2a57f5e0778db4cbbef4df7187ed1f4f0351
Houria <126777373+Hou-ry@users.noreply.github.com>
2025-05-03 23:46:59 +0100

    small changes for media root

--- travelProject/settings.py
+++ travelProject/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
