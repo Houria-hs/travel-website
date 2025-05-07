c5aa1666263638d517b6797606af0226ae08a12b
Houria <126777373+Hou-ry@users.noreply.github.com>
2025-05-07 22:08:41 +0100

    - Screenshots now visible through the Django admin panel

--- travelApp/admin.py
+++ travelApp/admin.py
@@ -7,5 +7,8 @@ admin.site.register(Destination)
 admin.site.register(Booking)
 admin.site.register(Subscribe)
 admin.site.register(PromoCode)
-admin.site.register(PayementProof)
 admin.site.register(Tarif)
+
+@admin.register(PayementProof)
+class PayementProofAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'phone', 'trip_title', 'transfer_date', 'screenshot')
